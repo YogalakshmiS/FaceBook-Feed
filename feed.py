dc17779257824e948f23544112081e0d261a01a2
Yogalakshmi S <76721591+YogalakshmiS@users.noreply.github.com>
2021-01-10 21:21:59 +0530

    python file

---
+++ feed.py
@@ -0,0 +1,6 @@
+import facebook
+
+page_access_token = "EAAQUo1z5pZC4BAC9wvgz8xbI9mZCNVkvmocLyKDYQtf9eUWr6x1RuDSWdXko2TZBJpv9kSLzw69omRFvPoiGcVUW57UDpZCvc343VCLSl6UapDTIw4fE5jWVZCEKgnno8EEW2JW9PAcPj9kMX2ZCBw3u4Tef65rxjxT7RZAGd3PKw6PUXs1gt3sM8WCnniHXLZCkRZCJ7GTLgI8KlU6KhurlJQVvEfGW1S1sZD"
+graph = facebook.GraphAPI(page_access_token)
+facebook_page_id = "1148591778932734"
+graph.put_object(facebook_page_id, "feed", message='Hello All')
